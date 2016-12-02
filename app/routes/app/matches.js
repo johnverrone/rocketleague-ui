398eccf61d9a4705a07bc85e750d389f577bda24
John Verrone <john.verrone@gmail.com>
2016-12-01 22:55:48 -0500

    changed static inputs to drop downs and date-picker

--- app/routes/app/matches.js
+++ app/routes/app/matches.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').findAll('match');
+    return RSVP.hash({
+      matches: this.get('store').findAll('match'),
+      teams: this.get('store').findAll('team')
+    });
   }
 });
